f45ff66cb3deb624c070ac9f7ddf16ce877b757e
Jennifer Bonapace <jennifer.bonapace@gmail.com>
2017-02-23 17:58:32 -0500

    J

--- controller.js
+++ controller.js
@@ -1,11 +1,12 @@
 var red = angular.module('red',[]);
 
-red.controller("ctrl", function($scope, $http) {
+red.controller("ctrl", function($scope, $http)
+
+{
   $http.get('https://www.reddit.com/r/aww/.json')
-  // .success(console.log("hello")
   .then(function(response){
     console.log(response);
-
-    // $scope.title=response.data;
+    var title=response.data.children[0].data.title;
+    console.log(title);
   });
 });
